1def51548d80204c078142451753248d53550759
AleksandraOrzechowska <aleksandraorzechowska11@wp.pl>
2022-01-28 22:43:36 +0100

    update admin for Books

--- books/admin.py
+++ books/admin.py
@@ -5,6 +5,9 @@ from .models import Book, Author, Tag
 
 
 class BookAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    list_filter = ("genre","tags","author",)
+    
     prepopulated_fields = {"slug" : ("title",)}
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
